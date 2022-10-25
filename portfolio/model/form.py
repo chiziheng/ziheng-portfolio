86774f281468779099c51712177cb24448eba146
chiziheng <zihengchi@gmail.com>
2022-10-25 11:37:11 +0800

    code refactor

--- portfolio/model/form.py
+++ portfolio/model/form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import InputRequired
 
+
 class ContactForm(FlaskForm):
     name = StringField("Name", validators=[InputRequired()])
     email = StringField("Email", validators=[InputRequired()])
